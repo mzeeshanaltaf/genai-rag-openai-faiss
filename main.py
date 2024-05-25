99a0b88461bd094c45dfc67338e9a7b5c2adcbb9
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-05-25 14:14:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,135 @@
+from util import *
+from streamlit_option_menu import option_menu
+from streamlit_extras.metric_cards import style_metric_cards
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+# --- PAGE CONFIGURATION ---
+st.set_page_config(page_title="PDF Genie", page_icon=":robot_face:", layout="centered")
+
+# --- SETUP SESSION STATE VARIABLES ---
+if "vector_store" not in st.session_state:
+    st.session_state.vector_store = False
+if "response" not in st.session_state:
+    st.session_state.response = None
+if "prompt_activation" not in st.session_state:
+    st.session_state.prompt_activation = False
+if "conversation" not in st.session_state:
+    st.session_state.conversation = None
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = None
+if "prompt" not in st.session_state:
+    st.session_state.prompt = False
+if "total_token" not in st.session_state:
+    st.session_state.total_token = 0
+if "successful_requests" not in st.session_state:
+    st.session_state.successful_requests = 0
+if "total_cost" not in st.session_state:
+    st.session_state.total_cost = 0.0
+
+# --- SIDEBAR CONFIGURATION ---
+api_key = sidebar_api_key_configuration()
+
+# --- MAIN PAGE CONFIGURATION ---
+st.title("PDF Genie :robot_face:")
+st.write("*Interrogate Documents :books:, Ignite Insights: AI at Your Service*")
+
+# ---- NAVIGATION MENU -----
+selected = option_menu(
+    menu_title=None,
+    options=["PDF Genie", "Analytics", "Reference", "About"],
+    icons=["robot", "bar-chart-fill", "bi-file-text-fill", "app"],  # https://icons.getbootstrap.com
+    orientation="horizontal",
+)
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the question based on the provided context only. If question is not within the context, do not try to answer
+    and respond that the asked question is out of context or something similar.
+    Please provide the most accurate response based on the question.
+    <context>
+    {context}
+    Questions: {input}
+    """
+)
+# ----- SETUP PDF GENIE MENU ------
+if selected == "PDF Genie":
+    st.subheader("Upload PDF(s)")
+    pdf_docs = st.file_uploader("Upload your PDFs", type=['pdf'], accept_multiple_files=True,
+                                disabled=not st.session_state.prompt_activation)
+    process = st.button("Process", type="primary", key="process", disabled=not pdf_docs)
+
+    llm = ChatOpenAI(openai_api_key=api_key, model="gpt-3.5-turbo")
+
+    if process:
+        with st.spinner("Processing ..."):
+            st.session_state.vector_store = create_vectorstore(pdf_docs)
+            st.session_state.prompt = True
+            st.success('Database is ready')
+
+    st.divider()
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    container = st.container(border=True)
+    if question := st.chat_input(placeholder='Enter your question related to uploaded document',
+                                 disabled=not st.session_state.prompt):
+        st.session_state.messages.append({"role": "user", "content": question})
+        st.chat_message("user").write(question)
+
+        with st.spinner('Processing...'):
+            st.session_state.response = get_llm_response(llm, prompt, question)
+            st.session_state.messages.append({"role": "assistant", "content": st.session_state.response['answer']})
+            st.chat_message("assistant").write(st.session_state.response['answer'])
+
+# ----- SETUP ANALYTICS MENU ------
+if selected == "Analytics":
+    st.title("Analytics")
+    st.write("*This page shows the analytics like total token used, total cost etc.*")
+    st.divider()
+    # Create metrics
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Token", f"{st.session_state.total_token}")
+    col2.metric("Total Requests", f"{st.session_state.successful_requests}")
+    col3.metric("Total Cost (USD)", f"${round(st.session_state.total_cost, 6)}")
+    style_metric_cards()
+
+# ----- SETUP REFERENCE MENU ------
+if selected == "Reference":
+    st.title("Reference & Context")
+    if st.session_state.response is not None:
+        for i, doc in enumerate(st.session_state.response["context"]):
+            with st.expander(f'Reference # {i + 1}'):
+                st.write(doc.page_content)
+
+# ----- SETUP ABOUT MENU ------
+if selected == "About":
+    with st.expander("About this App"):
+        st.markdown(''' This app allows you to chat with your PDF documents. It has following functionality:
+
+    - Allows to chat with multiple PDF documents
+    - Display the response context and document reference
+    - Display the analytics
+        ''')
+    with st.expander("Which Large Language models are supported by this App?"):
+        st.markdown(''' This app supports the following LLMs:
+
+    - Chat Model -- OpenAI gpt-3.5-turbo
+    - Embeddings -- OpenAI Text-embedding-ada-002-v2
+        ''')
+
+    with st.expander("Which library is used for vectorstore?"):
+        st.markdown(''' This app supports the FAISS for AI similarity search and vectorstore:
+        ''')
+
+    with st.expander("Where to get the source code of this app?"):
+        st.markdown(''' Source code is available at:
+    *  https://github.com/mzeeshanaltaf/genai-rag-openai-faiss
+        ''')
+    with st.expander("Whom to contact regarding this app?"):
+        st.markdown(''' Contact [Zeeshan Altaf](zeeshan.altaf@gmail.com)
+        ''')
---
+++ util.py
@@ -0,0 +1,64 @@
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.callbacks import get_openai_callback
+import streamlit as st
+
+
+# Function for API configuration at sidebar
+def sidebar_api_key_configuration():
+    st.sidebar.subheader("API Keys")
+    api_key = st.sidebar.text_input("Enter your API Key 🗝️", type="password",
+                                    help='Get API Key from: https://platform.openai.com/api-keys')
+    if api_key == '':
+        st.sidebar.warning('Enter the API Key 🗝️')
+        st.session_state.prompt_activation = False
+    elif api_key.startswith('sk-') and (len(api_key) == 51):
+        st.sidebar.success('Lets Proceed!', icon='️👉')
+        st.session_state.prompt_activation = True
+    else:
+        st.sidebar.warning('Please enter the correct API Key 🗝️!', icon='⚠️')
+        st.session_state.prompt_activation = False
+    return api_key
+
+
+# Read PDF data
+def read_pdf_data(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+# Split data into chunks
+def split_data(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_chunks = text_splitter.split_text(text)
+    return text_chunks
+
+
+# Create vectorstore
+def create_vectorstore(pdf_docs):
+    raw_text = read_pdf_data(pdf_docs)  # Get PDF text
+    text_chunks = split_data(raw_text)  # Get the text chunks
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+# Get response from llm of user asked question
+def get_llm_response(llm, prompt, question):
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retrieval_chain = create_retrieval_chain(st.session_state.vector_store.as_retriever(), document_chain)
+    with get_openai_callback() as cb:
+        response = retrieval_chain.invoke({'input': question})
+        st.session_state.total_token += cb.total_tokens
+        st.session_state.total_cost += cb.total_cost
+        st.session_state.successful_requests += cb.successful_requests
+        return response
+
